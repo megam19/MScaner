6a226c9c774dcc1b74c8b6b38dd137894a64201a
megam19 <megam.com@mail.ru>
2026-02-27 14:18:54 +0300

    Created structs

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"path/filepath"
 	"time"
 
 	_ "modernc.org/sqlite" // Важно: имя драйвера "sqlite"
@@ -13,18 +13,44 @@ import (
 
 // var DIR_PATH = "\\\\10.33.6.202\\na_prosmotr"
 // var DIR_PATH = "\\\\10.33.6.202\\lowres\\LOWRES_ARCHIVE"
-// var DIR_PATH = "\\\\fserver\\harris"
-var DIR_PATH = "\\\\air-02\\imagine_mxf"
+// var DIR_PATH = "\\\\air-02\\imagine_mxf"
+var DIR_PATH = "\\\\fserver\\harris"
 
 var database *sql.DB
-var timeSpeep = 60 //в секундах
+var queryPrepare *sql.Stmt
+var databasePath = "./database/sqlite3DB.db"
+var timeSpeep = 160 //в секундах
+var errDB error
+var arr_items_DBStruct []itemDBStruct
+var arr_items_FolderStruct []itemFolderStruct
+var i_db itemDBStruct
+var i_folder itemFolderStruct
+
+type itemDBStruct struct {
+	fileName string
+	fileSize int64
+}
+type itemFolderStruct struct {
+	fileName string
+	fileSize int64
+}
 
 func main() {
-	var errdb error
 	fmt.Println("Список файлов в: " + DIR_PATH)
-	database, errdb = sql.Open("sqlite", "./database/sqlite3DB.db")
-	if errdb != nil {
-		log.Fatalf("Ошибка открытия базы %d", errdb)
+	createDBifNotExists()
+
+	for {
+		readDatabase()
+		scanDir(DIR_PATH)
+		time.Sleep(time.Duration(timeSpeep) * time.Second)
+	}
+}
+
+func createDBifNotExists() {
+
+	database, errDB = sql.Open("sqlite", databasePath)
+	if errDB != nil {
+		log.Fatalf("Ошибка открытия базы %d", errDB)
 	}
 	defer database.Close()
 
@@ -35,39 +61,74 @@ func main() {
 						createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
 						updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
 					)`
-	statement, err := database.Prepare(queryCreateDB)
+	queryPrepare, err := database.Prepare(queryCreateDB)
 	if err != nil {
 		log.Fatalf("Ошибка при создании таблицы: %q", err)
 	}
-	statement.Exec()
-
-	for {
-		scanDir(DIR_PATH)
-		time.Sleep(time.Duration(timeSpeep) * time.Second)
-	}
+	queryPrepare.Exec()
 }
 
 func scanDir(dirPath string) {
-	files, err := os.ReadDir(dirPath)
+
+	files, err := os.ReadDir(dirPath) // Расчет на 20-40к файлов
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for _, file := range files {
-		if file.IsDir() {
+		if file.IsDir() && filepath.Ext(file.Name()) != ".mxf" {
 			continue
 		}
+
 		info, _ := file.Info()
 		size := info.Size() / 1024 //Преобразование в КБ
-		fSize := strconv.Itoa(int(size))
 
-		fmt.Println(file.Name() + " Размер: " + fSize + " КБ") // выременный вывод в консоль
-		writeDatabase(file.Name(), size)
+		if filepath.Ext(file.Name()) != ".mxf" { // Если папка и не mxf
+			fmt.Println("не MXF: " + file.Name())
+
+		}
+
+		i_folder = itemFolderStruct{file.Name(), size}                    //наполняем структуру
+		arr_items_FolderStruct = append(arr_items_FolderStruct, i_folder) // наполняем массив структурами
+
+		//writeDatabase(file.Name(), size)
+
 	}
+
+}
+
+func readDatabase() {
+	database, errDB = sql.Open("sqlite", databasePath)
+	if errDB != nil {
+		log.Fatal("База данных недоступен " + errDB.Error())
+	}
+	defer database.Close()
+
+	querySELECT := `SELECT fileName, fileSize FROM files;`
+	rows, errDB := database.Query(querySELECT)
+	if errDB != nil {
+		log.Fatalf("Ошибка запроса в базу данных")
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&i_db.fileName, &i_db.fileSize) // наполняем структуру
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		arr_items_DBStruct = append(arr_items_DBStruct, i_db) // наполняем массив структурами
+	}
+
+	for _, p := range arr_items_DBStruct {
+		fmt.Println(p.fileName, p.fileSize)
+	}
+	fmt.Printf("******************* Конец чтения Базы **********************")
+
 }
 
 func writeDatabase(fileName string, fileSize int64) {
-	database, _ = sql.Open("sqlite", "./database/sqlite3DB.db")
+	database, _ = sql.Open("sqlite", databasePath)
 	defer database.Close()
 
 	//Запрос записывает в случае отсутствует fileName или изменился размер файла.
